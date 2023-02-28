7b86c39f88227b611691ffb37c5e5092081874aa
alexm <alex-m@outlook.com>
2023-02-28 15:57:50 -0800

    Set up Flask app and main block for running app

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
 # Ioana A Mititean
 # 24.4 Exercise: Auth
 # Flask Feedback
+
+"""
+Flask app for user feedback: route and view definitions.
+"""
+
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db
+
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "O secreta secreta, of doamne"
+debug = DebugToolbarExtension(app)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback"
+# app.config['SQLALCHEMY_ECHO'] = True
+
+
+if __name__ == "__main__":
+
+    connect_db(app)
+
+    with app.app_context():
+        db.create_all()
+
+    app.run(host='127.0.0.1', port=5000, debug=True, threaded=False)
