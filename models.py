ea745158c1b4efecc821ba3a5b46176c37fd7817
alexm <alex-m@outlook.com>
2023-02-28 15:57:19 -0800

    Set up DB and function to connect Flask app to DB

--- models.py
+++ models.py
@@ -1,3 +1,20 @@
 # Ioana A Mititean
 # 24.4 Exercise: Auth
 # Flask Feedback
+
+"""
+Models for feedback app.
+"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """
+    Connect Flask app to SQLA database.
+    """
+
+    db.app = app
+    db.init_app(app)
