1587b9cbe4ccebf681190baa1c57363bb7b50040
alexm <alex-m@outlook.com>
2023-02-28 16:18:50 -0800

    Add routes, function defs, and docstrings for required view functions for app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
 Flask app for user feedback: route and view definitions.
 """
 
-from flask import Flask
+from flask import Flask, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db
@@ -20,6 +20,58 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback"
 # app.config['SQLALCHEMY_ECHO'] = True
+
+
+# ROUTES & VIEWS ----------------------------------------------------------------------------------
+
+@app.route("/")
+def display_home():
+    """
+    Homepage route.
+    """
+
+    return redirect("/register")
+
+
+@app.route("/register")
+def display_register_form():
+    """
+    Display a form for a user to register with.
+    """
+
+
+@app.route("/register", methods=["POST"])
+def register_user():
+    """
+    Handle user registration.
+    """
+
+    return redirect("/secret")
+
+
+@app.route("/login")
+def display_login_form():
+    """
+    Display a form for a user to login with.
+    """
+
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    """
+    Handle user login.
+    """
+
+
+@app.route("/secret")
+def display_secret():
+    """
+    Display a secret message.
+    """
+
+    return "You made it!"
+
+# -------------------------------------------------------------------------------------------------
 
 
 if __name__ == "__main__":
