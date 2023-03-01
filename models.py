c42aaba6754fa456fc324454f7f48b17acbe835e
alexm <alex-m@outlook.com>
2023-02-28 17:47:32 -0800

    Import and init Bcrypt; add table name to User model; write class method for registering a user and handling password encryption

--- models.py
+++ models.py
@@ -7,8 +7,10 @@ Models for feedback app.
 """
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 
 def connect_db(app):
@@ -25,8 +27,22 @@ class User(db.Model):
     Model for a user.
     """
 
+    __tablename__ = "users"
+
     username = db.Column(db.String(20), primary_key=True)
     password = db.Column(db.Text, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, password):
+        """
+        Register user with hashed password and return the user.
+        """
+
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+
+        # Return instance of user with username and hashed password
+        return cls(username=username, password=hashed_utf8)
