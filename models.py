3c3b971800f06ff4c535bed5e4ad1014745864f3
alexm <alex-m@outlook.com>
2023-02-28 16:03:02 -0800

    Define User model and columns

--- models.py
+++ models.py
@@ -18,3 +18,15 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class User(db.Model):
+    """
+    Model for a user.
+    """
+
+    username = db.Column(db.String(20), primary_key=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
