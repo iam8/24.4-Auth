b139214c66fde48f9616f7c6cfdde00c6771748a
alexm <alex-m@outlook.com>
2023-03-02 18:24:19 -0800

    Define models for forms for adding and updating feedback

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ Form model creation and setup.
 """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField
+from wtforms import StringField, PasswordField, EmailField, TextAreaField
 from wtforms.validators import InputRequired
 
 
@@ -49,3 +49,27 @@ class LoginUserForm(FlaskForm):
     password = PasswordField("Password",
                              validators=[InputRequired()],
                              render_kw={"placeholder": "Enter your password"})
+
+
+class AddFeedbackForm(FlaskForm):
+    """
+    Model for the form a user will fill out to add a new feedback.
+    """
+
+    title = StringField("Title",
+                        validators=[InputRequired()],
+                        render_kw={"placeholder": "Enter a title"})
+
+    content = TextAreaField("Content",
+                            validators=[InputRequired()],
+                            render_kw={"placeholder": "Enter your feedback"})
+
+
+class UpdateFeedbackForm(FlaskForm):
+    """
+    Model for the form a user will fill out to update an existing feedback.
+    """
+
+    content = TextAreaField("Content",
+                            validators=[InputRequired()],
+                            render_kw={"placeholder": "Enter your feedback"})
