03f885ad37093764dd0597719b141077a0e11a8e
alexm <alex-m@outlook.com>
2023-03-02 17:03:59 -0800

    Update seed file to add some users and feedback data

--- seed.py
+++ seed.py
@@ -6,8 +6,11 @@
 Seed file for Feedback app - clears the database.
 """
 
-from models import db, connect_db
+from models import db, connect_db, User, Feedback
 from app import app
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 
 if __name__ == "__main__":
@@ -17,3 +20,36 @@ if __name__ == "__main__":
     with app.app_context():
         db.drop_all()
         db.create_all()
+
+        pw1 = bcrypt.generate_password_hash("dragon1").decode("utf8")
+        pw2 = bcrypt.generate_password_hash("dragon2").decode("utf8")
+
+        a1 = User(username="a1",
+                  password=pw1,
+                  email="a1@a1.com",
+                  first_name="a1",
+                  last_name="mit1")
+
+        a2 = User(username="a2",
+                  password=pw2,
+                  email="a2@a2.com",
+                  first_name="a2",
+                  last_name="mit2")
+
+        db.session.add_all([a1, a2])
+        db.session.commit()
+
+        fb1 = Feedback(title="FB1 by a1",
+                       content="Example feedback 1",
+                       username="a1")
+
+        fb2 = Feedback(title="FB2 by a2",
+                       content="Example feedback 2",
+                       username="a2")
+
+        fb3 = Feedback(title="FB3 by a2",
+                       content="Example feedback 3",
+                       username="a2")
+
+        db.session.add_all([fb1, fb2, fb3])
+        db.session.commit()
